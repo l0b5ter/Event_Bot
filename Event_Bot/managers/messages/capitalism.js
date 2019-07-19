13702bd59cda6c9f8df41c02f02005d05a9e84bb
l0b5ter <44732631+l0b5ter@users.noreply.github.com>
2019-07-19 14:06:15 +0200

    Delete capitalism.js

--- Event_Bot/managers/messages/capitalism.js
+++
@@ -1,9 +0,0 @@
-const ChannelFilter = require('./channelFilter');
-
-class Capitalism extends ChannelFilter {
-    getName() {
-        return 'capitalism';
-    }
-}
-
-module.exports = Capitalism;
